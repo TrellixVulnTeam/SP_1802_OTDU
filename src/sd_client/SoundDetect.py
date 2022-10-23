3cb5edefcf2d6d5cfff12faeb7cd619e33b767a7
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 19:01:57 +0000

    Adding tarfile member sanitization to extractall()

--- src/sd_client/SoundDetect.py
+++ src/sd_client/SoundDetect.py
@@ -173,7 +173,26 @@ def getSdk(deviceID):
 
     # expand tar
     with tarfile.open('sd_sdk.tar.gz', 'r') as tar:
-        tar.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar)
     os.remove('sd_sdk.tar.gz')
 
 ###############################################################################
